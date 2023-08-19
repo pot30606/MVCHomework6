bd0cd1ba1095ce393eaa0eeb75ae128f180f122a
郭耀文 <pot30606@pic.net.tw>
2023-08-19 10:54:47 +0800

    修正tag命名方式

--- Helper/Tag/TagsTagHelper.cs
+++ Helper/Tag/TagsTagHelper.cs
@@ -8,23 +8,18 @@ using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using MVCHomework6.Data.Database;
 
-
 namespace MVCHomework6.Helper.Tag
 {
-    public class FakeImgHelper : TagHelper
+    public class TagsTagHelper : TagHelper
     {
-
         private IUrlHelperFactory UrlHelperFactory { get; }
         private IActionContextAccessor Accessor { get; }
 
-        [HtmlAttributeName("my-width")]
-        public string? Width { get; set; }
-        [HtmlAttributeName("my-height")]
-        public string? Height { get; set; }
-        //https://unsplash.it/200/200/?random
-        public readonly string Url = "https://unsplash.it/WIDTH/HEIGHT/?random";
+        [HtmlAttributeName("tag-name")]
+        public string? TagName { get; set; }
+        public readonly string UrlPrefix = "/?q=";
 
-        public FakeImgHelper(IUrlHelperFactory urlHelperFactory, IActionContextAccessor accessor)
+        public TagsTagHelper(IUrlHelperFactory urlHelperFactory, IActionContextAccessor accessor)
         {
             UrlHelperFactory = urlHelperFactory;
             Accessor = accessor;
@@ -34,11 +29,11 @@ namespace MVCHomework6.Helper.Tag
         {
             var actionContext = Accessor.ActionContext;
             var urlHelper = UrlHelperFactory.GetUrlHelper(actionContext);
-            output.TagName = "img";
-            string _url = Url.Replace("HEIGHT", Height ?? "200").Replace("WIDTH", Width ?? "200");
-            output.Attributes.SetAttribute("src", _url);
-            output.Attributes.SetAttribute("alt", $"這是一張假圖{Width}x{Height}");
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", UrlPrefix + TagName);
+            output.Content.SetHtmlContent(TagName);
         }
 
     }
+
 }
